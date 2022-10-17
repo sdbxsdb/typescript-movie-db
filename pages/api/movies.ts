232b0fb2e0598fc4feb17ff645484bc81f773d67
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-17 16:23:29 +0100

    fetching data from api

---
+++ pages/api/movies.ts
@@ -0,0 +1,26 @@
+//API  URLS
+import { SEARCH_BASE_URL, POPULAR_BASE_URL } from "../../config";
+
+//BASIC FETCH
+import { basicFetch } from "../../api/fetchFunctions";
+
+//TYPES
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Movies } from "../../api/types";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Movies>
+) {
+  const { page, search } = req.query; //Get search params
+
+  const endpoint = search
+    ? `${SEARCH_BASE_URL}${search}&page=${page}`
+    : `${POPULAR_BASE_URL}&${page}`;
+
+
+    const data = await basicFetch<Movies>(endpoint);
+
+    res.status(200).json(data);
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import type { NextPage } from 'next';
-import Header from '../components/Header/Header';
+import {useState} from 'react';
+import type { NextPage } from "next";
+//Fetch hook
+import {useFetchMovies} from "../api/fetchHooks";
 
-
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Grid from "../components/Grid/Grid";
+import Card from "../components/Card/Card";
+import Spinner from "../components/Spinner/Spinner";
 
 const Home: NextPage = () => {
+  const [query, setQuery] = useState("");
+
+  const {data, fetchNextPage, isLoading, isFetching, error} = useFetchMovies(query);
+
+  console.log(data);
   return (
     <main className="relative h-screen overflow-y-scroll">
-      <Header/>
+      <Header />
+      <Hero />
+      <Grid />
+      <Card />
+      <Spinner />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
