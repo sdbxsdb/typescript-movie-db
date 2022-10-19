5b4f66659987b7c6544bf84055d6badeba7bc83f
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-10-19 10:56:16 +0100

    getstaticprops and paths created

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -8,9 +8,16 @@ import MovieInfo from "../components/MovieInfo/MovieInfo";
 import Grid from '../components/Grid/Grid';
 import Card from '../components/Card/Card';
 
-const Movie: NextPage = () => (
+type Props = {
+  movie: Movie;
+  directors: Crew[];
+  cast: Cast[];
+}
+
+const Movie: NextPage<Props> = ({ movie, directors, cast}) => (
   <main>
     <Header/>
+    <Breadcrumb/>
     <MovieInfo/>
     <Grid>
       <Card/>
@@ -19,3 +26,32 @@ const Movie: NextPage = () => (
 )
 
 export default Movie;
+
+export const getStaticProps: GetStaticProps = async context => {
+  const id = context.params?.id as string;
+
+  const movieEndpoint: string = movieUrl(id);
+  const creditsEndpoint: string = creditsUrl(id);
+
+  const movie = await basicFetch<Movie>(movieEndpoint);
+  const credits = await basicFetch<Credits>(creditsEndpoint);
+
+  // Get the directors only
+  const directors = credits.crew.filter(member => member.job === "Director");
+
+  return { 
+    props: {
+      movie,
+      directors,
+      cast: credits.cast
+    },
+    revalidate: 60 * 60 * 24 // re-build page every 24 hours
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
